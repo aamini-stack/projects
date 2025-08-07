7758c5252da0bacb3bb94a0f25ea9f0ddf996dc3
Aria <amini5454@gmail.com>
2025-08-07 16:45:02 -0400

    More fixes

--- apps/imdbgraph/src/components/search-bar.test.tsx
+++ apps/imdbgraph/src/components/search-bar.test.tsx
@@ -1,6 +1,6 @@
 import { actions } from 'astro:actions'
 import { QueryClient } from '@tanstack/react-query'
-import { render, screen } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import { expect, test, vi } from 'vitest'
 import { searchMocks } from './__fixtures__/search'
@@ -23,6 +23,7 @@ vi.mock(import('@/lib/query'), () => ({
 }))
 
 test('basic search', async () => {
+	const user = userEvent.setup()
 	vi.mocked(actions.fetchSuggestions).mockResolvedValue({
 		data: searchMocks.avatar,
 		error: undefined,
@@ -30,12 +31,13 @@ test('basic search', async () => {
 
 	render(<SearchBar />)
 	const searchBar = await screen.findByRole('combobox')
-	userEvent.type(searchBar, 'avatar')
+	await user.type(searchBar, 'avatar')
 	expect(await screen.findByTestId('loading-spinner')).toBeVisible()
 	expect(await screen.findByText(/Avatar: The Last Airbender/i)).toBeVisible()
 })
 
 test('no results', async () => {
+	const user = userEvent.setup()
 	vi.mocked(actions.fetchSuggestions).mockResolvedValue({
 		data: [],
 		error: undefined,
@@ -43,33 +45,23 @@ test('no results', async () => {
 
 	render(<SearchBar />)
 	const searchBar = await screen.findByRole('combobox')
-	userEvent.type(searchBar, 'blah')
+	await user.type(searchBar, 'blah')
 	expect(await screen.findByTestId('loading-spinner')).toBeVisible()
 	expect(await screen.findByText(/No TV Shows Found./i)).toBeVisible()
 })
 
-// test('error message', async () => {
-// 	vi.mocked(actions.fetchSuggestions).mockRejectedValue({
-// 		data: undefined,
-// 		error: new Error(),
-// 	})
+test('error message', async () => {
+	vi.mocked(actions.fetchSuggestions).mockRejectedValue({
+		data: undefined,
+		error: new Error(),
+	})
 
-// 	render(<SearchBar />)
-// 	const searchBar = await screen.findByRole('combobox')
-// 	userEvent.type(searchBar, 'blah')
-// 	await waitFor(async () => expect(await screen.findByText('')).toBeVisible())
-// 	// await waitFor(() => 	expect(
-// 	// 	await screen.findByText(
-// 	// 		/Something went wrong. Please try again./i,
-// 	// 		{},
-// 	// 		{ timeout: 20_000 },
-// 	// 	),
-// 	// ).toBeVisible())
-// 	// expect(
-// 	// 	await screen.findByText(
-// 	// 		/Something went wrong. Please try again./i,
-// 	// 		{},
-// 	// 		{ timeout: 20_000 },
-// 	// 	),
-// 	// ).toBeVisible()
-// })
+	render(<SearchBar />)
+	const searchBar = await screen.findByRole('combobox')
+	userEvent.type(searchBar, 'blah')
+	await waitFor(async () =>
+		expect(
+			await screen.findByText(/Something went wrong. Please try again./i),
+		).toBeVisible(),
+	)
+})
