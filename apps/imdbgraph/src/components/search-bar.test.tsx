8aad87bfbd8d4b0ed735d91801f49f4117f839a5
Aria Amini <57912635+aamini11@users.noreply.github.com>
2025-08-07 17:32:09 -0400

    Search bar bug (#7)

--- apps/imdbgraph/src/components/search-bar.test.tsx
+++ apps/imdbgraph/src/components/search-bar.test.tsx
@@ -1,55 +1,67 @@
-import { ActionError } from 'astro:actions'
-import { fireEvent, render, screen } from '@testing-library/react'
+import { actions } from 'astro:actions'
+import { QueryClient } from '@tanstack/react-query'
+import { render, screen, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import { expect, test, vi } from 'vitest'
 import { searchMocks } from './__fixtures__/search'
 import { SearchBar } from './search-bar'
 
 vi.mock('astro:actions', () => ({
 	actions: {
-		fetchSuggestions: mockFetchSuggestions,
+		fetchSuggestions: vi.fn(),
 	},
 }))
 
-test('loading spinner', async () => {
-	render(<SearchBar />)
+vi.mock(import('@/lib/query'), () => ({
+	queryClient: new QueryClient({
+		defaultOptions: {
+			queries: {
+				retry: false,
+			},
+		},
+	}),
+}))
 
+test('basic search', async () => {
+	const user = userEvent.setup()
+	vi.mocked(actions.fetchSuggestions).mockResolvedValue({
+		data: searchMocks.avatar,
+		error: undefined,
+	})
+
+	render(<SearchBar />)
 	const searchBar = await screen.findByRole('combobox')
-	fireEvent.change(searchBar, { target: { value: 'blah' } })
+	await user.type(searchBar, 'avatar')
 	expect(await screen.findByTestId('loading-spinner')).toBeVisible()
+	expect(await screen.findByText(/Avatar: The Last Airbender/i)).toBeVisible()
 })
 
 test('no results', async () => {
-	render(<SearchBar />)
+	const user = userEvent.setup()
+	vi.mocked(actions.fetchSuggestions).mockResolvedValue({
+		data: [],
+		error: undefined,
+	})
 
+	render(<SearchBar />)
 	const searchBar = await screen.findByRole('combobox')
-	fireEvent.change(searchBar, { target: { value: 'blah' } })
-	expect(await screen.findByText(/No TV Shows Found./i)).toBeInTheDocument()
+	await user.type(searchBar, 'blah')
+	expect(await screen.findByTestId('loading-spinner')).toBeVisible()
+	expect(await screen.findByText(/No TV Shows Found./i)).toBeVisible()
 })
 
 test('error message', async () => {
+	vi.mocked(actions.fetchSuggestions).mockRejectedValue({
+		data: undefined,
+		error: new Error(),
+	})
+
 	render(<SearchBar />)
-
 	const searchBar = await screen.findByRole('combobox')
-	fireEvent.change(searchBar, { target: { value: 'error' } })
-	expect(
-		await screen.findByText(
-			/Something went wrong. Please try again./i,
-			{},
-			{ timeout: 20_000 },
-		),
-	).toBeInTheDocument()
-}, 20_000)
-
-function mockFetchSuggestions({ query }: { query: string }) {
-	if (query === 'error') {
-		return Promise.resolve({
-			error: new ActionError({
-				code: 'INTERNAL_SERVER_ERROR',
-			}),
-		})
-	} else {
-		return Promise.resolve({
-			data: searchMocks[query.toLocaleLowerCase()] ?? [],
-		})
-	}
-}
+	userEvent.type(searchBar, 'blah')
+	await waitFor(async () =>
+		expect(
+			await screen.findByText(/Something went wrong. Please try again./i),
+		).toBeVisible(),
+	)
+})
