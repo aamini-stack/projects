c413e445dbb1c0bbdceadda43ffc7dbb37a095ef
Aria <amini5454@gmail.com>
2025-08-09 18:52:20 -0400

    Rename

--- apps/imdbgraph-e2e/playwright.config.ts
+++ apps/imdbgraph-e2e/playwright.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
 		colorScheme: 'dark',
 
 		/* Base URL to use in actions like `await page.goto('/')`. */
-		baseURL: baseUrl,
+		baseURL: 'https://imdbgraph-rmyibhvb5-aamini.vercel.app/',
 
 		/* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
 		trace: 'on-first-retry',
