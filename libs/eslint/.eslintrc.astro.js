275b8e208f5639c721401dc6e35e9a8424510c0d
Aria <amini5454@gmail.com>
2025-08-16 19:07:50 -0400

    wip

--- libs/eslint/.eslintrc.astro.js
+++ libs/eslint/.eslintrc.astro.js
@@ -1,3 +1,5 @@
+// @ts-check
+
 import { defineConfig, globalIgnores } from 'eslint/config'
 import eslintPluginAstro from 'eslint-plugin-astro'
 import eslintPluginJsxA11y from 'eslint-plugin-jsx-a11y'
@@ -16,26 +18,39 @@ export default defineConfig([
 	...baseConfig,
 	{
 		files: ['**/*.{js,mjs,cjs,jsx,mjsx,ts,tsx,mtsx}'],
-		...pluginReact.configs.flat.recommended,
+		// ...pluginReact.configs.flat.recommended,
+		// ...pluginReact.configs.flat['jsx-runtime'],
+		// // ...eslintPluginJsxA11y.flatConfigs.recommended,
+		// ...pluginReactHooks.configs['recommended-latest'],
+		settings: { react: { version: 'detect' } },
 		languageOptions: {
 			...pluginReact.configs.flat.recommended.languageOptions,
+			// ...eslintPluginJsxA11y.flatConfigs.recommended.languageOptions,
 			globals: {
 				...globals.serviceworker,
+				...globals.browser,
 			},
 		},
 	},
-	{
-		files: ['**/*.{js,mjs,cjs,jsx,mjsx,ts,tsx,mtsx}'],
-		plugins: {
-			'react-hooks': pluginReactHooks,
-		},
-		settings: { react: { version: 'detect' } },
-		rules: {
-			...pluginReactHooks.configs.recommended.rules,
-			// React scope no longer necessary with new JSX transform.
-			'react/react-in-jsx-scope': 'off',
-		},
-	},
-	...eslintPluginAstro.configs.recommended,
-	...eslintPluginAstro.configs['jsx-a11y-strict'],
+	...eslintPluginAstro.configs['flat/recommended'],
+	...eslintPluginAstro.configs['flat/jsx-a11y-strict'],
+	// {
+	// 	files: ['**/*.astro'],
+	// 	plugins: {
+	// 		'jsx-a11y': eslintPluginJsxA11y.configs.strict.plugins,
+	// 	},
+	// 	languageOptions: {
+	// 		parserOptions: {
+	// 			ecmaFeatures: {
+	// 				jsx: true,
+	// 			},
+	// 		},
+	// 	},
+	// 	rules: {
+	// 		// ... any rules you want
+	// 		'jsx-a11y/alt-text': 'error',
+	// 	},
+	// },
+	// ...eslintPluginAstro.configs['flat/recommended'],
+	// ...eslintPluginAstro.configs['flat/jsx-a11y-strict'],
 ])
