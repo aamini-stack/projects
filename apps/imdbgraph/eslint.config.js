142c5761a00106183dda9f0f01e4d3750d2f04bb
Aria <amini5454@gmail.com>
2025-08-17 03:34:30 -0400

    add eslint

--- apps/imdbgraph/eslint.config.js
+++ apps/imdbgraph/eslint.config.js
@@ -1,13 +1,4 @@
 import base from '@aamini/config-eslint/base'
 import pluginTanstack from '@tanstack/eslint-plugin-query'
-import { defineConfig } from 'eslint/config'
 
-/** @type {import('eslint').Linter.Config} */
-export default defineConfig([
-	...base,
-	...pluginTanstack.configs['flat/recommended'],
-	{
-		// Override rules here...
-		rules: {},
-	},
-])
+export default [...base, pluginTanstack.configs['flat/recommended']]
---
+++ apps/portfolio/eslint.config.js
@@ -0,0 +1,3 @@
+import base from '@aamini/config-eslint/base'
+
+export default [...base]
