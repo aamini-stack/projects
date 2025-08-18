ae3b96355e1dcbbc23833327128ed06d165aedb0
Aria <amini5454@gmail.com>
2025-08-18 15:46:31 -0400

    Fix

--- apps/dota-visualizer/vitest.config.ts
+++ apps/dota-visualizer/vitest.config.ts
@@ -4,7 +4,7 @@ import { getViteConfig } from 'astro/config'
 export default getViteConfig({
 	test: {
 		include: ['src/**/*.{test,spec}.?(c|m)[jt]s?(x)', '!tests/**'],
-		setupFiles: 'vitest.setup.ts',
+		setupFiles: ['__mocks__/setup-http.ts', '__mocks__/setup-jest-dom.ts'],
 		globals: true,
 		environment: 'jsdom',
 		includeSource: ['src/**/*.{js,ts}'],
