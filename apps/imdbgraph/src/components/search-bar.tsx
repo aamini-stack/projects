224dffa4ab2d9b713cb795ad706b2cfa52b22cbe
Aria <amini5454@gmail.com>
2025-08-23 20:52:41 -0400

    reformat

--- apps/imdbgraph/src/components/search-bar.tsx
+++ apps/imdbgraph/src/components/search-bar.tsx
@@ -93,15 +93,7 @@ export function SearchBar() {
 			</label>
 
 			{/* Search Bar */}
-			<div
-				className={cn(
-					'border-input shadow-xs selection:bg-primary selection:text-primary-foreground placeholder:text-muted-foreground dark:bg-input/30 flex h-10 w-full min-w-0 items-center rounded-xl border bg-transparent px-3 py-1 text-base outline-none transition-[color,box-shadow] md:text-sm',
-					'has-focus-visible:border-ring has-focus-visible:ring-[3px] has-focus-visible:ring-ring/50',
-					'has-disabled:pointer-events-none has-disabled:cursor-not-allowed has-disabled:opacity-50',
-					'has-aria-invalid:border-destructive has-aria-invalid:ring-destructive/20 dark:has-aria-invalid:ring-destructive/40',
-					'file:text-foreground file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium',
-				)}
-			>
+			<div className="border-input shadow-xs selection:bg-primary selection:text-primary-foreground placeholder:text-muted-foreground dark:bg-input/30 has-focus-visible:border-ring has-focus-visible:ring-[3px] has-focus-visible:ring-ring/50 has-disabled:pointer-events-none has-disabled:cursor-not-allowed has-disabled:opacity-50 has-aria-invalid:border-destructive has-aria-invalid:ring-destructive/20 dark:has-aria-invalid:ring-destructive/40 file:text-foreground flex h-10 w-full min-w-0 items-center rounded-xl border bg-transparent px-3 py-1 text-base outline-none transition-[color,box-shadow] file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium md:text-sm">
 				<SearchIcon className="mr-2 h-4 w-4 shrink-0 opacity-50" />
 				<input
 					aria-invalid={!!error}
@@ -163,9 +155,7 @@ export function SearchBar() {
 						<li
 							key={show.imdbId}
 							className={cn(
-								'text-foreground/60 w-full cursor-pointer select-none rounded-md px-2 py-1.5 text-sm outline-none',
-								'disabled:pointer-events-none disabled:opacity-50',
-								'[&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0',
+								'text-foreground/60 w-full cursor-pointer select-none rounded-md px-2 py-1.5 text-sm outline-none disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0',
 								{
 									'opacity-50': isFetching,
 									'bg-accent text-accent-foreground':
