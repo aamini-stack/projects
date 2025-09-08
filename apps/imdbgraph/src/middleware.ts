d64bb741eccf56084c2e5d4ca7aa85f69ac7bdbc
aamini <amini5454@gmail.com>
2025-09-07 22:54:40 -0400

    rm

--- apps/imdbgraph/src/middleware.ts
+++
@@ -1,6 +0,0 @@
-import { handleRequest } from '@aamini/utils/proxy'
-import type { MiddlewareHandler } from 'astro'
-
-export const onRequest: MiddlewareHandler = (context, next) => {
-	return handleRequest(context, next)
-}
