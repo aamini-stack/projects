8cf447fb7047910c3812262567153093fc7ca856
aamini <amini5454@gmail.com>
2025-09-10 02:50:34 -0400

    Fix

--- apps/imdbgraph/src/middleware.ts
+++ apps/imdbgraph/src/middleware.ts
@@ -1,8 +1,6 @@
 import { handleRequest, config as vercelConfig } from '@aamini/utils/proxy'
 import { defineMiddleware } from 'astro:middleware'
 
-export const onRequest = defineMiddleware((context, next) => {
-	return handleRequest(context, next)
-})
+export const onRequest = defineMiddleware(handleRequest)
 
 export const config = vercelConfig
