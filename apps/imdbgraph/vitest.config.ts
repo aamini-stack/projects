c9ec03296471cc67baf7501b4a36879f13c3cb94
aamini <amini5454@gmail.com>
2025-09-28 18:46:02 -0400

    Minor tweaks

--- apps/imdbgraph/vitest.config.ts
+++ apps/imdbgraph/vitest.config.ts
@@ -10,7 +10,7 @@ export default mergeConfig(
 					extends: true,
 					test: {
 						name: 'db',
-						include: ['src/**/*.test.db.ts'],
+						include: ['src/**/*.db.test.ts'],
 						setupFiles: ['./__mocks__/setup-db.ts'],
 						testTimeout: 30_000,
 					},
