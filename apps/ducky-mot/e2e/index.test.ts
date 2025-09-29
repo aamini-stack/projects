46b203d3856ea193c812a4222c93b4becdb179a2
aamini <amini5454@gmail.com>
2025-09-29 04:45:05 -0400

    Fix tests

--- apps/ducky-mot/e2e/index.test.ts
+++ apps/ducky-mot/e2e/index.test.ts
@@ -1,6 +1,9 @@
 import { expect, test } from '@playwright/test'
 
 test.beforeEach(async ({ page }) => {
+	// Disable images for more stable tests.
+	await page.route('**/*.{png,jpg,jpeg,gif}', (route) => route.abort())
+
 	await page.goto('/')
 })
 
