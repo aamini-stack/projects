0c2f1573e4e736967edf03f84c6e7fcd56e78f76
aamini <amini5454@gmail.com>
2025-09-30 00:17:14 -0400

    Add video

--- packages/config-testing/playwright.shared.config.ts
+++ packages/config-testing/playwright.shared.config.ts
@@ -39,6 +39,7 @@ export const baseConfig = (
 				/* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
 				trace: 'on-first-retry',
 				screenshot: 'only-on-failure',
+				video: 'retain-on-failure',
 
 				// https://vercel.com/docs/deployment-protection/methods-to-bypass-deployment-protection/protection-bypass-automation#examples
 				extraHTTPHeaders: {
