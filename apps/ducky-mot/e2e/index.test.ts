2e04e4ff79b2a06360bbba765403935abe012419
aamini <amini5454@gmail.com>
2025-10-07 21:37:45 -0400

    Remove flaky logo

--- apps/ducky-mot/e2e/index.test.ts
+++ apps/ducky-mot/e2e/index.test.ts
@@ -2,7 +2,7 @@ import { expect, test } from '@playwright/test'
 
 test.beforeEach(async ({ page }) => {
 	// Disable images for more stable tests.
-	await page.route('**/*.{png,jpg,jpeg,gif}', (route) => route.abort())
+	await page.route('**/*.{png,jpg,jpeg,gif,svg}', (route) => route.abort())
 	// Block YouTube domains
 	await page.route(/.*youtube\.com.*/, (route) => route.abort())
 	await page.goto('/')
