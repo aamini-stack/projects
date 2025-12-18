d2f0698fd475d623e3d7d433598cdf531d3db95d
aamini <amini5454@gmail.com>
2025-12-18 06:14:14 -0500

    Stabilize tests

--- apps/pc-tune-ups/e2e/index.test.ts
+++ apps/pc-tune-ups/e2e/index.test.ts
@@ -1,14 +1,16 @@
 import { expect, test } from '@playwright/test'
 
 test.beforeEach(async ({ page }) => {
-	await page.goto('/')
+	await page.goto('/', { waitUntil: 'networkidle' })
 })
 
 test('Screenshot Homepage', async ({ page }) => {
+	await page.waitForLoadState('networkidle')
 	await expect(page).toHaveScreenshot()
 })
 
 test('Screenshot Homepage (Full)', async ({ page }) => {
+	await page.waitForLoadState('networkidle')
 	await expect(page).toHaveScreenshot({
 		fullPage: true,
 	})
