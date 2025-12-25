dda860aa1fef013f3b820e2fd3d2d1d33c5e8195
aamini <amini5454@gmail.com>
2025-12-25 15:12:43 -0500

    Fix reporter

--- packages/config-testing/playwright.shared.config.ts
+++ packages/config-testing/playwright.shared.config.ts
@@ -24,7 +24,7 @@ export const baseConfig = (
 			forbidOnly: !!process.env.CI,
 			retries: process.env.CI ? 3 : 0,
 			/* Reporter to use. See https://playwright.dev/docs/test-reporters */
-			reporter: process.env.CI ? 'github' : 'html',
+			reporter: [['html', { open: 'never' }]],
 
 			/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
 			use: {
