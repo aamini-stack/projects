0558f9c93d9a71e4c1c2906b079badd48243bac8
Aria Amini <amini5454@gmail.com>
2026-02-14 05:09:41 -0600

    Fix typeerror

--- apps/imdbgraph/drizzle.config.ts
+++ apps/imdbgraph/drizzle.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'drizzle-kit'
 import { loadEnv } from 'vite'
 
 const env = loadEnv('production', '.', '')
+if (!env.DATABASE_URL) {
+	throw Error('Missing DATABASE_URL')
+}
 
 export default defineConfig({
 	schema: './src/db/tables.ts',
