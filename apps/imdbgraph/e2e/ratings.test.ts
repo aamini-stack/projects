9a75f5dab5608d69462dc2f30d7ca17297f438d8
Aria Amini <amini5454@gmail.com>
2026-04-18 23:27:31 -0400

    safe

--- apps/imdbgraph/e2e/ratings.test.ts
+++ apps/imdbgraph/e2e/ratings.test.ts
@@ -1,7 +1,7 @@
 import { expect, test } from '@playwright/test'
 
 test('Ratings page title works', async ({ page }) => {
-	await page.goto('/ratings/tt0944947', { waitUntil: 'networkidle' })
+	await page.goto('/ratings/tt0944947')
 	await expect(
 		page.getByRole('heading', { name: 'Game of Thrones' }),
 	).toBeVisible()
