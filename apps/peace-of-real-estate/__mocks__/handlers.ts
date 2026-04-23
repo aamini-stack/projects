ef1c158acfad975752e3fe211ec8acae616b8979
Aria Amini <amini5454@gmail.com>
2026-04-23 14:58:45 -0400

    delete e2e tests

--- apps/peace-of-real-estate/__mocks__/handlers.ts
+++ apps/peace-of-real-estate/__mocks__/handlers.ts
@@ -1,8 +1,1 @@
-import suggestions from '@/mocks/data/suggestions.json' with { type: 'json' }
-import { http, HttpResponse } from 'msw'
-
-export default [
-	http.get('/api/suggestions', () => {
-		return HttpResponse.json(suggestions)
-	}),
-]
+export default []
