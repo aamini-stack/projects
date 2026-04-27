04e50c976a17d79db09b5aa1e8c6115bd04e2bc5
Aria Amini <amini5454@gmail.com>
2026-04-27 17:31:15 -0400

    wip

--- apps/peace-of-real-estate/src/env.ts
+++ apps/peace-of-real-estate/src/env.ts
@@ -1,18 +1,18 @@
 import { z } from 'zod'
 
 const schema = z.object({
-	DATABASE_URL: z.string().min(1).optional(),
-	BETA_PASSWORD: z.string().min(1).optional(),
-	BETTER_AUTH_URL: z.string().min(1),
-	BETTER_AUTH_SECRET: z.string().min(1).optional(),
-	OAUTH_PROXY_SECRET: z.string().min(1).optional(),
-	GOOGLE_CLIENT_ID: z.string().min(1).optional(),
-	GOOGLE_CLIENT_SECRET: z.string().min(1).optional(),
-	AWS_REGION: z.string().min(1),
-	AVATAR_BUCKET: z.string().min(1).optional(),
-	AWS_ENDPOINT_URL: z.string().min(1).optional(),
-	AWS_ACCESS_KEY_ID: z.string().min(1).optional(),
-	AWS_SECRET_ACCESS_KEY: z.string().min(1).optional(),
+	DATABASE_URL: z.string().optional(),
+	BETA_PASSWORD: z.string().optional(),
+	BETTER_AUTH_URL: z.string(),
+	BETTER_AUTH_SECRET: z.string().optional(),
+	OAUTH_PROXY_SECRET: z.string().optional(),
+	GOOGLE_CLIENT_ID: z.string().optional(),
+	GOOGLE_CLIENT_SECRET: z.string().optional(),
+	AWS_REGION: z.string(),
+	AVATAR_BUCKET: z.string().optional(),
+	AWS_ENDPOINT_URL: z.string().optional(),
+	AWS_ACCESS_KEY_ID: z.string().optional(),
+	AWS_SECRET_ACCESS_KEY: z.string().optional(),
 })
 
 export const env = schema.parse(process.env)
