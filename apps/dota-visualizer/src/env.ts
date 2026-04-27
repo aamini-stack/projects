51b7fa8a52433018959e973eb80f3c192cb3d84f
Aria Amini <amini5454@gmail.com>
2026-04-27 19:55:13 -0400

    env wip

--- apps/dota-visualizer/src/env.ts
+++ apps/dota-visualizer/src/env.ts
@@ -1,4 +1,12 @@
+import { config } from 'dotenv'
 import { z } from 'zod'
+
+const environmentName = process.env.RAILWAY_ENVIRONMENT_NAME ?? process.env.NODE_ENV ?? 'development'
+
+config({
+	path: [`.env.${environmentName}`, '.env'],
+	quiet: true,
+})
 
 const schema = z.object({})
 
